3b729c213f53b80b5b13b4d9e43659a646fcb14b
Xubin Cao <cxb@swufe.edu.cn>
2024-01-31 14:32:56 +0800

    add hello.py

---
+++ 01_hello/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Author  : Xubin
+email   : cxb@swufe.edu.cn
+Date    : 2024-01-21
+Purpose : Study hoo to write a python code more format and good.
+"""
+
+print("hello, good!")
